d4d1994ef452b1241f00bb88d43a7e4afee4234e
Julio Vieira <juliocesarjuniorjja@hotmail.com>
2023-05-31 18:19:47 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('form-atividade');
+const nomes = [];
+const numeros = [];
+
+let  linhas = ''; 
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    adicionaLinha(); 
+    atualizaTabela();
+    apagaTfoot()
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    if (nomes.includes(inputNomeContato.value)) {
+        alert(`${inputNomeContato.value} já está na agenda, insira outro nome`);
+    } else {
+        nomes.push(inputNomeContato.value);
+        numeros.push(parseFloat(inputNumeroContato.value));
+        
+        let linha = '<tr>'; 
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputNumeroContato.value}</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;
+    }
+
+    inputNomeContato.value = ''; 
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function apagaTfoot() {
+    document.getElementById('sem-contatos').style.display = 'none';
+}
